152b5b49ed45c9a99b6b58f2ee906739288c053c
Emmanuel Ogbuzuru <58727101+Brag2gr8@users.noreply.github.com>
2023-06-05 15:47:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+
+
+
+// import { defineConfig } from 'vite'
+//  import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//  plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import { VitePluginRedirect } from 'vite-plugin-redirect';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    VitePluginRedirect({
+      // Redirect all routes to index.html
+      // "*" matches all paths
+      from: '.*',
+      to: '/index.html',
+    }),
+  ],
+});
