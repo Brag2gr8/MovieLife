ae785cbcf3bf71eaadb80fb61bc1587ed1bfc2c1
Brag2gr8 <brag2gr8@gmail.com>
2023-06-11 23:11:19 +0100

    update version
    added features to the app

--- src/pages/CreateWatchlist.jsx
+++ src/pages/CreateWatchlist.jsx
@@ -1,80 +1,99 @@
-import { useEffect, useState } from "react"
-import { useNavigate, useOutletContext } from "react-router-dom"
-import { nanoid } from "nanoid"
-
+import { useEffect, useState } from "react";
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 export default function CreateWatchList() {
-    const [currentWatchlist, setCurrentWatchlist] = useState([])
-    const { setRefresh } = useOutletContext()
-    const [formData, setFormData] = useState({
-        id: nanoid(),
-        name: "",
-        description: "",
-        movies: []
-    })
-    const navigate = useNavigate()
-    
-    useEffect(() => {
-        const existingWatchList = localStorage.getItem("allWatchlist")
-        const data = JSON.parse(existingWatchList)
+  // State to hold the current watchlist data
+  const [currentWatchlist, setCurrentWatchlist] = useState([]);
 
-        if(data) {
-            setCurrentWatchlist(data)
-        }
+  // Accessing the refresh function from the outlet context
+  const { setRefresh } = useOutletContext();
 
-        
-    }, [currentWatchlist.length])
+  // State to hold the form data for creating a new watchlist
+  const [formData, setFormData] = useState({
+    id: nanoid(),
+    name: "",
+    description: "",
+    movies: [],
+  });
 
-    function handleChange(e) {
-        const {name, value} = e.target
+  // Navigate function from react-router-dom to handle routing
+  const navigate = useNavigate();
 
-        setFormData(prev => {
-            return {
-                ...prev,
-                [name]: value
-            }
-        })
+  // useEffect hook to load existing watchlist data from local storage
+  useEffect(() => {
+    const existingWatchList = localStorage.getItem("allWatchlist");
+    const data = JSON.parse(existingWatchList);
+
+    if (data) {
+      setCurrentWatchlist(data);
+    }
+  }, [currentWatchlist.length]);
+
+  // Event handler for input changes in the form
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+
+  // Event handler for form submission
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Check if the watchlist name already exists
+    const isExistingWatchlist = currentWatchlist.some(
+      (watchlist) => watchlist.name === formData.name
+    );
+
+    // If the watchlist name already exists, show an alert and return early
+    if (isExistingWatchlist) {
+      alert("Watchlist name already exists. Please choose a different name.");
+      return;
     }
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        alert(`${formData.name} WatchList successfully created`)
-        setRefresh(prev => !prev)
-        const updatedWatchlist= [...currentWatchlist, formData];
-        setCurrentWatchlist(updatedWatchlist)
-        localStorage.setItem('allWatchlist', JSON.stringify(updatedWatchlist))
-        navigate("/")
-    }
+    // If the watchlist name is unique, show success alert, update watchlist data, and navigate to the home page
+    alert(`${formData.name} WatchList successfully created`);
+    setRefresh((prev) => !prev);
 
-    return (
-        <div className="create-watchlist-page">
-            <form onSubmit={handleSubmit} className="create-watchlist-form">
-                <label>
-                    Name
-                    <input 
-                        className="create-watchlist-input"
-                        type= "text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <label>
-                    Description
-                    <textarea 
-                        className="create-watchlist-textarea"
-                        type= "text"
-                        name="description"
-                        value={formData.description}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <button className="create-watchlist-button">
-                    Create Watchlist
-                </button>
-            </form>
-        </div>
-    )
+    const updatedWatchlist = [...currentWatchlist, formData];
+    setCurrentWatchlist(updatedWatchlist);
+    localStorage.setItem("allWatchlist", JSON.stringify(updatedWatchlist));
+    navigate("/");
+  }
+
+  return (
+    <div className="create-watchlist-page">
+      <form onSubmit={handleSubmit} className="create-watchlist-form">
+        <label>
+          Name
+          <input
+            className="create-watchlist-input"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            className="create-watchlist-textarea"
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button className="create-watchlist-button">Create Watchlist</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,88 +1,96 @@
-/* eslint-disable react-refresh/only-export-components */
-import { Link, useLoaderData } from "react-router-dom"
+import { Link, useLoaderData } from "react-router-dom";
 import {
-    getTrendingMovies,
-    getPopularMovies,
-    getNowPlayingMovies,
-    getTopRatedMovies,
-    getUpcomingMovies
-} from "../../../utils"
-import MovieCard from "../../components/MovieCard"
+  getTrendingMovies,
+  getPopularMovies,
+  getNowPlayingMovies,
+  getTopRatedMovies,
+  getUpcomingMovies
+} from "../../../utils";
+import MovieCard from "../../components/MovieCard";
 
+// Loader function to fetch movie data
 export async function loader() {
-    
-    const {returnedMovies: popularMovies} = await getPopularMovies()
-    const {returnedMovies: trendingMovies} = await getTrendingMovies()
-    const {returnedMovies: nowPlayingMovies} = await getNowPlayingMovies()
-    const {returnedMovies: topRatedMovies} = await getTopRatedMovies()
-    const {returnedMovies: upcomingMovies} = await getUpcomingMovies()
+  const { returnedMovies: popularMovies } = await getPopularMovies();
+  const { returnedMovies: trendingMovies } = await getTrendingMovies();
+  const { returnedMovies: nowPlayingMovies } = await getNowPlayingMovies();
+  const { returnedMovies: topRatedMovies } = await getTopRatedMovies();
+  const { returnedMovies: upcomingMovies } = await getUpcomingMovies();
 
-    return {
-        popularMovies, 
-        trendingMovies, 
-        topRatedMovies, 
-        nowPlayingMovies, 
-        upcomingMovies
-    }
-
-    console.log(popularMovies)
+  return {
+    popularMovies,
+    trendingMovies,
+    topRatedMovies,
+    nowPlayingMovies,
+    upcomingMovies
+  };
 }
 
-
 export default function Movies() {
+  // Accessing the loader data using useLoaderData hook
+  const {
+    popularMovies,
+    trendingMovies,
+    topRatedMovies,
+    nowPlayingMovies,
+    upcomingMovies
+  } = useLoaderData();
 
-    const {
-        popularMovies, 
-        trendingMovies, 
-        topRatedMovies, 
-        nowPlayingMovies, 
-        upcomingMovies
-    } = useLoaderData()
-
-    console.log(popularMovies)
-    
-    function renderMovie(movieType, movieHeader) {
-        const movieTray = movieType.slice(0, 10).map(movie => {
-            return <MovieCard
-                key={movie.id}
-                id={movie.id}
-                name={movie.name}
-                rating={movie.rating}
-                image={movie.image}
-                year={movie.year}
-            />
-        })
-        
-        return (
-            <>
-                <div className="movie-type" >
-                    <h2>{movieHeader}</h2>
-                </div>
-                <section className="movie-tray">
-                {movieTray}
-                </section>
-            </>
-        )
-    }
+  // Function to render 10 movies in movie tray
+  function renderMovie(movieType, movieHeader) {
+    const movieTray = movieType.slice(0, 10).map((movie) => {
+      return (
+        <MovieCard
+          key={movie.id}
+          id={movie.id}
+          name={movie.name}
+          rating={movie.rating}
+          image={movie.image}
+          year={movie.year}
+        />
+      );
+    });
 
     return (
-        <div className="movies">
-            <div className="movies-list-container">
-                <Link to="/movies/trending"><span>Trending</span></Link>
-                <Link to="/movies/popular"><span>Popular</span></Link>
-                <Link to= "/movies/now-playing"><span>Now playing</span></Link>
-                <Link to= "/movies/top-rated"><span>Top rated</span></Link>
-                <Link to="/movies/upcoming"><span>Upcoming</span></Link>
-            </div>
-            {renderMovie(trendingMovies, "TRENDING")}
-            <hr/>
-            {renderMovie(popularMovies, "POPULAR")}
-            <hr/>
-            {renderMovie(topRatedMovies, "TOP RATED")}
-            <hr/>
-            {renderMovie(nowPlayingMovies, "NOW PLAYING")}
-            <hr/>
-            {renderMovie(upcomingMovies, "UPCOMING")}
+      <>
+        <div className="movie-type">
+          <h2>{movieHeader}</h2>
         </div>
-    )
+        <section className="movie-tray">{movieTray}</section>
+      </>
+    );
+  }
+
+  return (
+    <div className="movies">
+      <div className="movies-list-container">
+        {/* Links to different movie categories */}
+        <Link to="/movies/trending">
+          <span>Trending</span>
+        </Link>
+        <Link to="/movies/popular">
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/now-playing">
+          <span>Now playing</span>
+        </Link>
+        <Link to="/movies/top-rated">
+          <span>Top rated</span>
+            </Link>
+        <Link to="/movies/upcoming">
+          <span>Upcoming</span>
+        </Link>
+      </div>
+
+      {/* Render movie trays for each category */}
+      {renderMovie(trendingMovies, "TRENDING")}
+      <hr />
+      {renderMovie(popularMovies, "POPULAR")}
+      <hr />
+      {renderMovie(topRatedMovies, "TOP RATED")}
+      <hr />
+      {renderMovie(nowPlayingMovies, "NOW PLAYING")}
+      <hr />
+      {renderMovie(upcomingMovies, "UPCOMING")}
+    </div>
+  );
 }
