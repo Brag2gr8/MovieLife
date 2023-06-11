8e30a55a8559182d5acbe33ede6063847dba78ec
Emmanuel Ogbuzuru <58727101+Brag2gr8@users.noreply.github.com>
2023-06-11 23:30:22 +0100

    Update Movies.jsx

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -58,31 +58,42 @@ export default function Movies() {
                 <div className="movie-type" >
                     <h2>{movieHeader}</h2>
                 </div>
-                <section className="movie-tray">
-                {movieTray}
-                </section>
-            </>
-        )
-    }
-
-    return (
-        <div className="movies">
-            <div className="movies-list-container">
-                <Link to="/movies/trending"><span>Trending</span></Link>
-                <Link to="/movies/popular"><span>Popular</span></Link>
-                <Link to= "/movies/now-playing"><span>Now playing</span></Link>
-                <Link to= "/movies/top-rated"><span>Top rated</span></Link>
-                <Link to="/movies/upcoming"><span>Upcoming</span></Link>
-            </div>
-            {renderMovie(trendingMovies, "TRENDING")}
-            <hr/>
-            {renderMovie(popularMovies, "POPULAR")}
-            <hr/>
-            {renderMovie(topRatedMovies, "TOP RATED")}
-            <hr/>
-            {renderMovie(nowPlayingMovies, "NOW PLAYING")}
-            <hr/>
-            {renderMovie(upcomingMovies, "UPCOMING")}
-        </div>
+                  <section className="movie-tray">{movieTray}</section>
+      </>
     );
+  }
+
+  return (
+    <div className="movies">
+      <div className="movies-list-container">
+        {/* Links to different movie categories */}
+        <Link to="/movies/trending">
+          <span>Trending</span>
+        </Link>
+        <Link to="/movies/popular">
+          <span>Popular</span>
+        </Link>
+        <Link to="/movies/now-playing">
+          <span>Now playing</span>
+        </Link>
+        <Link to="/movies/top-rated">
+          <span>Top rated</span>
+            </Link>
+        <Link to="/movies/upcoming">
+          <span>Upcoming</span>
+        </Link>
+      </div>
+
+      {/* Render movie trays for each category */}
+      {renderMovie(trendingMovies, "TRENDING")}
+      <hr />
+      {renderMovie(popularMovies, "POPULAR")}
+      <hr />
+      {renderMovie(topRatedMovies, "TOP RATED")}
+      <hr />
+      {renderMovie(nowPlayingMovies, "NOW PLAYING")}
+      <hr />
+      {renderMovie(upcomingMovies, "UPCOMING")}
+    </div>
+  );
 }
