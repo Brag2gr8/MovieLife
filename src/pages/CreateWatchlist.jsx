30f145e6f68c40943bbfcb86a6c3f943bc61471e
Emmanuel Ogbuzuru <58727101+Brag2gr8@users.noreply.github.com>
2023-06-12 00:09:23 +0100

    Update CreateWatchlist.jsx

--- src/pages/CreateWatchlist.jsx
+++ src/pages/CreateWatchlist.jsx
@@ -10,7 +10,6 @@ export default function CreateWatchList() {
   // Accessing the refresh function from the outlet context
   const { setRefresh } = useOutletContext();
 
-  
   // State to hold the form data for creating a new watchlist
   const [formData, setFormData] = useState({
     id: nanoid(),
@@ -22,16 +21,15 @@ export default function CreateWatchList() {
   // Navigate function from react-router-dom to handle routing
   const navigate = useNavigate();
     
-    useEffect(() => {
-        const existingWatchList = localStorage.getItem("allWatchlist")
-        const data = JSON.parse(existingWatchList)
+// useEffect hook to load existing watchlist data from local storage
+  useEffect(() => {
+    const existingWatchList = localStorage.getItem("allWatchlist");
+    const data = JSON.parse(existingWatchList);
 
-        if(data) {
-            setAllWatchlist(data)
-        }
-
-        
-    }, [])
+    if (data) {
+      setCurrentWatchlist(data);
+    }
+  }, [currentWatchlist.length]);
 
     function handleChange(e) {
         const {name, value} = e.target
@@ -44,52 +42,58 @@ export default function CreateWatchList() {
         })
     }
 
-    function handleSubmit(e) {
-        const isInWatchlist = allWatchlist.some(el => formData.name === el.name)
-        
-        if (!isInWatchlist) {
-            console.log(isInWatchlist)
-            e.preventDefault()
-            alert(`${formData.name} WatchList successfully created`)
-            setRefresh(prev => !prev)
-            const updatedWatchlist= [...allWatchlist, formData];
-            setAllWatchlist(updatedWatchlist)
-            localStorage.setItem('allWatchlist', JSON.stringify(updatedWatchlist))
-            navigate("/")
-        } else {
-            alert("watchlist name already exists, choose another name")
-        }
+  // Event handler for form submission
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // Check if the watchlist name already exists
+    const isExistingWatchlist = currentWatchlist.some(
+      (watchlist) => watchlist.name === formData.name
+    );
+
+    // If the watchlist name already exists, show an alert and return early
+    if (isExistingWatchlist) {
+      alert("Watchlist name already exists. Please choose a different name.");
+      return;
     }
 
-    return (
-        <div className="create-watchlist-page">
-            <form onSubmit={handleSubmit} className="create-watchlist-form">
-                <label>
-                    Name
-                    <input 
-                        className="create-watchlist-input"
-                        type= "text"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <label>
-                    Description
-                    <textarea 
-                        className="create-watchlist-textarea"
-                        type= "text"
-                        name="description"
-                        value={formData.description}
-                        onChange={handleChange}
-                        required
-                    />
-                </label>
-                <button className="create-watchlist-button">
-                    Create Watchlist
-                </button>
-            </form>
-        </div>
-    )
+    // If the watchlist name is unique, show success alert, update watchlist data, and navigate to the home page
+    alert(`${formData.name} WatchList successfully created`);
+    setRefresh((prev) => !prev);
+
+    const updatedWatchlist = [...currentWatchlist, formData];
+    setCurrentWatchlist(updatedWatchlist);
+    localStorage.setItem("allWatchlist", JSON.stringify(updatedWatchlist));
+    navigate("/");
+  }
+
+  return (
+    <div className="create-watchlist-page">
+      <form onSubmit={handleSubmit} className="create-watchlist-form">
+        <label>
+          Name
+          <input
+            className="create-watchlist-input"
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <label>
+          Description
+          <textarea
+            className="create-watchlist-textarea"
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button className="create-watchlist-button">Create Watchlist</button>
+      </form>
+    </div>
+  );
 }
