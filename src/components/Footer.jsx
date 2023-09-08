f3a49e1912325366032b361ba068cc6abf6f8c65
Emmanuel Ogbuzuru <58727101+Brag2gr8@users.noreply.github.com>
2023-09-08 12:59:44 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,16 +5,16 @@ const Footer = () => (
             <img className="tmbd-logo" src={tmbd} alt="tmbd logo" />
         </a>
         <div className="social-icons-container">
-            <a href="http://twitter.com/bragthefirst" target="_blank">
+            {/* <a href="http://twitter.com/dev_bragg" target="_blank">
                 <i className="fa-brands fa-twitter"></i>
-            </a>
+            </a> */}
             <a href="http://linkedin.com/in/brag2" target="_blank">
                 <i className="fa-brands fa-linkedin"></i>
             </a>
             <a href="http://github.com/brag2gr8" target="_blank">
                 <i className="fa-brands fa-github"></i>
             </a>
-            <a href="http://mailto:brag2gr8@gmail.com" target="_blank">
+            <a href="mailto:brag2gr8@gmail.com?subject=Inquiry&body=Hello%20Emmanuel!" target="_blank">
                 <i className="fa-solid fa-envelope"></i>
             </a>
         </div>
